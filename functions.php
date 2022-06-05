f43875a84449af7d47eb18d91618ba475de6b762
Tia <104393269+tiadotdev@users.noreply.github.com>
2022-06-05 12:24:57 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,5 +3,5 @@
 // Add Custom JS File
 add_action( 'wp_enqueue_scripts', 'purplefish_custom_scripts', 999999 );
 function purplefish_custom_scripts() {
-    wp_enqueue_script('script', get_stylesheet_directory_uri() . '/script.js', array( 'jquery' ), rand());
+    wp_enqueue_script('script', get_stylesheet_directory_uri() . '/assets/js/script.js', array( 'jquery' ), rand());
 }
